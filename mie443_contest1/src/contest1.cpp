21f32e11239cbb01e5704daa0e5ee58cca458af4
Darie Roman <darieroman10@gmail.com>
2021-01-24 02:43:18 +0000

    starting point

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -12,12 +12,12 @@
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-	ROS_INFO("hello github!");
+
 }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-	//fill with your code
+	
 }
 
 int main(int argc, char **argv)
