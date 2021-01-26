1ffcfce8a869fafa196416cd8287e33ac69dcd78
Darie Roman <darieroman10@gmail.com>
2021-01-26 01:26:58 +0000

    tut2 implemented in oop

--- mie443_contest1/include/main_node_class.hpp
+++ mie443_contest1/include/main_node_class.hpp
@@ -3,6 +3,8 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
 
 #include <boost/thread.hpp>
 
@@ -10,6 +12,14 @@
 #include <cmath>
 
 #include <chrono>
+
+#define N_BUMPER 3
+#define RAD2DEG(rad) ((rad)* 180./M_PI)
+#define DEG2RAD(deg) ((deg)* M_PI/180.)
+
+#ifndef DESIRED_ANGLE
+#define DESIRED_ANGLE 5
+#endif
 
 namespace mainSpace {
 
@@ -24,12 +34,19 @@ private:
 
     ros::Subscriber bumper_sub_;
     ros::Subscriber laser_sub_;
+    ros::Subscriber odom_sub_;
     ros::Publisher vel_pub_;
 
     geometry_msgs::Twist vel_;
 
-    float angular_;
-    float linear_;
+    float angular_, linear_;
+    float posX_, posY_, yaw_;
+
+    uint8_t bumper_[3] = {kobuki_msgs::BumperEvent::RELEASED, 
+                         kobuki_msgs::BumperEvent::RELEASED, 
+                         kobuki_msgs::BumperEvent::RELEASED};
+
+    float minLaserDist_;
 
     // contest count down timer
     std::chrono::time_point<std::chrono::system_clock> start_;
@@ -46,6 +63,8 @@ public:
     void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg);
 
     void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
 
     void timerCallback(const ros::TimerEvent& event);
 
--- mie443_contest1/src/main_node_class.cpp
+++ mie443_contest1/src/main_node_class.cpp
@@ -11,11 +11,12 @@ MainNodeClass::MainNodeClass(ros::NodeHandle& node_handle, ros::NodeHandle& priv
 
 void MainNodeClass::init() 
 {
-    angular_ = 0.0;
-    linear_ = 0.0;
+    angular_ = linear_ = 0.0;
+    posX_ = posY_ = yaw_ = 0.0;
 
     bumper_sub_ = nh_.subscribe("mobile_base/events/bumper", 10, &MainNodeClass::bumperCallback, this);
     laser_sub_ = nh_.subscribe("scan", 10, &MainNodeClass::laserCallback, this);
+    odom_sub_ = nh_.subscribe("odom", 1, &MainNodeClass::odomCallback, this);
     vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
     timer_ = pnh_.createTimer(ros::Duration(0.1), &MainNodeClass::timerCallback, this);
@@ -26,17 +27,73 @@ void MainNodeClass::init()
 
 void MainNodeClass::bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-    //fill with your code
+    bumper_[msg->bumper] = msg->state;
 }
 
 void MainNodeClass::laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    //fill with your code
+    minLaserDist_ = std::numeric_limits<float>::infinity();
+	int32_t nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
+    int32_t desiredNLasers = DEG2RAD(DESIRED_ANGLE) / msg->angle_increment;
+    ROS_INFO("Size of laser scan array: %iand size of offset: %i", nLasers, desiredNLasers);
+
+    if (DEG2RAD(DESIRED_ANGLE) < msg->angle_max && -DEG2RAD(DESIRED_ANGLE) > msg->angle_min) {
+        for (uint32_t laser_idx = nLasers/2 - desiredNLasers; laser_idx < nLasers/2 + desiredNLasers; laser_idx++) {
+            minLaserDist_ = std::min(minLaserDist_, msg->ranges[laser_idx]);
+            }
+        }
+            else {
+                for (uint32_t laser_idx = 0; laser_idx < nLasers; laser_idx++) {
+                    minLaserDist_ = std::min(minLaserDist_, msg->ranges[laser_idx]);
+                    }
+                }
+}
+
+void MainNodeClass::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+	posX_ = msg->pose.pose.position.x;
+    posY_ = msg->pose.pose.position.y;
+    yaw_ = tf::getYaw(msg->pose.pose.orientation);
+    
+    ROS_INFO("Position: (%f,%f) Orientation:%frad or%fdegrees.", posX_, posY_, yaw_, RAD2DEG(yaw_));
 }
 
 void MainNodeClass::timerCallback(const ros::TimerEvent &event)
 {
-    //this is the code that will loop
+    bool any_bumper_pressed = false;
+    for (uint32_t b_idx = 0; b_idx < N_BUMPER; b_idx++) {
+        any_bumper_pressed |= (bumper_[b_idx] == kobuki_msgs::BumperEvent::PRESSED);
+    }
+
+    // Control logic after bumpers are being pressed.
+    if (posX_<0.5 && yaw_<M_PI/12 && !any_bumper_pressed) {
+        angular_ = 0.0;
+        linear_ = 0.2;
+    }
+    else if (yaw_<M_PI/2 && posX_>0.5 && !any_bumper_pressed) {
+        angular_ = M_PI/6;
+        linear_ = 0.0;
+    }
+    else if (minLaserDist_>1. && !any_bumper_pressed) {
+        linear_ = 0.1;
+        if(yaw_<17/36*M_PI || posX_>0.6) {
+            angular_ = M_PI/12.;
+        }
+        else if (yaw_<19/36*M_PI || posX_<0.4) {
+            angular_ = -M_PI/12.;
+        }
+        else {
+            angular_ = 0;
+        }
+    }
+    else {
+        angular_ = 0.0;
+        linear_ = 0.0;
+    } 
+
+    vel_.angular.z = angular_;
+    vel_.linear.x = linear_;
+    vel_pub_.publish(vel_);
 
     //update the timer
     secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start_).count();
