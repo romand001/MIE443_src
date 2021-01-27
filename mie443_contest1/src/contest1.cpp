20746354b716302688dae23f507123dbba6c7f28
Yaakob613 <yaakob.bendayan@gmail.com>
2021-01-26 21:06:25 -0500

    tut2 additions

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -3,21 +3,46 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
 
 #include <stdio.h>
 #include <cmath>
 
 #include <chrono>
 
+#define N_BUMPER (3)
+#define RAD2DEG(rad)((rad)* 180./M_PI)
+#define DEG2RAD(deg)((deg)* M_PI/180.)
+
+
+float angular = 0.0;
+float linear = 0.0;
+float posX = 0.0, posY = 0.0, yaw = 0.0;
+uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+
+float minLaserDist = std::numeric_limits<float>::infinity();
+int32_t nLasers=0, desiredNLasers=0, desiredAngle=5;
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-	//fill with your code
+	// Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
+    bumper[msg->bumper] =msg->state;
 }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-	//fill with your code
+	nLasers=(msg->angle_max-msg->angle_min) /msg->angle_increment;
+    desiredNLasers=DEG2RAD(desiredAngle)/msg->angle_increment;
+    ROS_INFO("Size of laser scan array: %iand size of offset: %i", nLasers, desiredNLasers);
+}
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+	posX=msg->pose.pose.position.x;
+    posY=msg->pose.pose.position.y;
+    yaw=tf::getYaw(msg->pose.pose.orientation);
+    tf::getYaw(msg->pose.pose.orientation);ROS_INFO("Position: (%f,%f) Orientation:%frad or%fdegrees.", posX, posY,yaw,RAD2DEG(yaw));
 }
 
 int main(int argc, char **argv)
@@ -27,6 +52,7 @@ int main(int argc, char **argv)
 
     ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
     ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
+    ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback);
 
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
@@ -44,7 +70,32 @@ int main(int argc, char **argv)
 
     while(ros::ok() && secondsElapsed <= 900) {
         ros::spinOnce();
-        //fill with your code
+        //
+        // check bumper message
+        // ROS_INFO("bumperLEFT: %d", bumper[0]);
+        // ROS_INFO("bumperCENTRE: %d", bumper[1]);
+        // ROS_INFO("bumperRIGHT: %d", bumper[2]);
+
+        // Check if any of the bumpers were pressed.
+        /* bool any_bumper_pressed = false;
+        for(uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx) {
+            any_bumper_pressed |= (bumper[b_idx] ==kobuki_msgs::BumperEvent::PRESSED);
+            }
+        //
+        // Control logic after bumpers are being pressed.
+        if(posX<0.5 && yaw <M_PI /12 &&! any_bumper_pressed) {
+            angular =0.0;
+            linear =0.2;
+        }
+        else if(yaw <M_PI /2 && posX>0.5 &&! any_bumper_pressed) {
+            angular =M_PI /6;
+            linear =0.0;
+        }
+        else{
+            angular =0.0;
+            linear =0.0;
+            break;
+        }*/ 
 
         vel.angular.z = angular;
         vel.linear.x = linear;
