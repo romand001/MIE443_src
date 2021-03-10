0e9f52394abc3dc4627cf4fb54b16fe3e68e9c3d
Darie Roman <darieroman10@gmail.com>
2021-03-09 19:18:52 -0500

    getting descriptors of tags and scene

--- mie443_contest2/include/imagePipeline.h
+++ mie443_contest2/include/imagePipeline.h
@@ -6,14 +6,24 @@
 #include <cv.h>
 #include <cv_bridge/cv_bridge.h>
 #include <boxes.h>
+#include<iostream>
+
+#include"opencv2/calib3d.hpp"
+#include"opencv2/highgui.hpp"
+#include"opencv2/imgproc.hpp"
+#include"opencv2/features2d.hpp"
+#include"opencv2/xfeatures2d.hpp"
 
 class ImagePipeline {
     private:
         cv::Mat img;
         bool isValid;
         image_transport::Subscriber sub;
+
+        std::vector<Mat> tagDescriptors;
     public:
         ImagePipeline(ros::NodeHandle& n);
         void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+        void setTagDescriptors();
         int getTemplateID(Boxes& boxes);
 };
